ed6ade82ce59f777fcb50e661a498b6f1d8b4d43
Jonghoe <ninebell94@gmail.com>
2020-08-29 15:30:10 +0900

    conflict

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from torch.utils.tensorboard import SummaryWriter
+import tqdm
 import torch
+import abc
 
 
 class TorchBoard:
@@ -19,30 +21,42 @@ class TorchBoard:
         self.writer.add_scalar('Acc/test', value, n_iter)
 
 
+class BaseDataLoader:
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def data_load(self):
+        pass
+    
+
 def train_model(epoches, model, loss, optim, train_loader, validate_loader, save_path, tag=None, checkpoint=None):
     tb = TorchBoard(save_path, tag)
     for epoch in range(1, epoches+1):
         train_loss = 0
         validate_loss = 0
-        for iter, (x, y) in enumerate(train_loader):
+        for iter, value in tqdm.tqdm(enumerate(train_loader.data_load())):
+            x, y = value
             optim.zero_grad()
             result = model(x)
             iter_loss = loss(y, result)
             train_loss += iter_loss
             iter_loss.backward()
             optim.step()
+            if iter > 10:
+                break
         train_loss /= iter
 
-        with torch.no_grade():
-            for iter, (x, y) in enumerate(validate_loader):
-                optim.zero_grad()
-                result = model(x)
-                iter_loss = loss(y, result)
-                validate_loss += iter_loss
-        validate_loss /= iter
+        if validate_loader is not None:
+            with torch.no_grade():
+                for iter, (x, y) in enumerate(validate_loader):
+                    optim.zero_grad()
+                    result = model(x)
+                    iter_loss = loss(y, result)
+                    validate_loss += iter_loss
+            validate_loss /= iter
+            tb.add_test_loss(validate_loss.item(), epoch)
 
-        tb.add_train_loss(train_loss, epoch)
-        tb.add_validate_loss(validate_loss, epoch)
+        tb.add_train_loss(train_loss.item(), epoch)
 
         if checkpoint is not None:
             checkpoint(model, train_loss, validate_loss)
